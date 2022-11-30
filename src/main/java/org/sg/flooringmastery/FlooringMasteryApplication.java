fd33e20c7cafbf52dfb661abbe85a87249b67ec6
joshuaschoemmell <jrs@joshuaschoemmell.com>
2022-11-30 09:57:20 -0600

    init

---
+++ src/main/java/org/sg/flooringmastery/FlooringMasteryApplication.java
@@ -0,0 +1,13 @@
+package org.sg.flooringmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlooringMasteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlooringMasteryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sg/flooringmastery/FlooringMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sg.flooringmastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlooringMasteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
